a2580edf7ab8223fcd4138248b6e5d53baea73db
eXiz7 <veizxell@gmail.com>
2019-12-02 03:10:17 +0200

    initial commit

---
+++ Assets/Scripts/MsgPanel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MsgPanel : MonoBehaviour
+{
+    public event Action<string> OnSendMessage = delegate { };
+
+    [SerializeField] private Transform _panelContent;
+    [SerializeField] private InputField _input;
+    [SerializeField] private Button _sendMsg;
+    [SerializeField] private Text _msgPrefab;
+
+    void Start()
+    {
+        _sendMsg.onClick.RemoveAllListeners();
+        _sendMsg.onClick.AddListener(() =>
+        {
+            OnSendMessage.Invoke(_input.text);
+            _input.text = string.Empty;
+        });
+    }
+
+    public void CreateMsg(string msg)
+    {
+        var obj = Instantiate(_msgPrefab);
+        obj.text = msg;
+        obj.transform.SetParent(_panelContent, false);
+        obj.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SocketIO;
+using UnityEngine;
+
+[RequireComponent(typeof(SocketIOComponent))]
+public class NetworkController : MonoBehaviour
+{
+    private SocketIOComponent _socketComponent;
+    private MsgPanel _msgPanel;
+
+    private void Start()
+    {
+        _socketComponent = GetComponent<SocketIOComponent>();
+        _msgPanel = FindObjectOfType<MsgPanel>();
+        _msgPanel.OnSendMessage += _msgPanel_OnSendMessage;
+        ConnectToServer();
+    }
+
+    private void ConnectToServer()
+    {
+        _socketComponent.Connect();
+        _socketComponent.On("say_hi", OnHiCallback);
+    }
+
+    private void OnHiCallback(SocketIOEvent args)
+    {
+        _msgPanel.CreateMsg(args.data["msg"].ToString());
+    }
+
+    private void _msgPanel_OnSendMessage(string msg)
+    {
+        var data = new Dictionary<string, string>();
+        data.Add("msg", msg);
+        _socketComponent.Emit("say_hi", new JSONObject(data));
+    }
+}
